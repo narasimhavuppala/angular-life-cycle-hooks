523f037649dac653fa42a17b153a93700fa7d7ca
nraovuppala@gmail.com <nraovuppala@gmail.com>
2017-09-24 22:56:31 +0530

    First vommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  name = 'Alice';
+  logs: string[] = [];
+  showChild = true;
+
+  ngOnInit() {
+    setTimeout(() => this.updateName(), 3000);
+    setTimeout(() => this.hideChild(), 4000);
+  }
+
+  updateName() {
+    this.name = 'Bob';
+  }
+
+  hideChild() {
+    this.showChild = false;
+    this.logs.push(`onDestroy`);
+  }
+
+  onLog(data) {
+    this.logs.push(data);
+  }
 }
